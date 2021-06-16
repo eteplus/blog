aab7c8b74e4424c532f7e29d6430ef074c8e033e
eteplus <eteplus@outlook.com>
2021-06-16 17:22:34 +0800

    feat: add some templates and macros

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss'),
+    require('postcss-nested'),
     require('autoprefixer'),
     process.env.NODE_ENV === 'production'
       ? require('cssnano')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,36 @@
 module.exports = {
-	purge: {
-    content: [
-      './templates/**/*.html', './theme/**/*.html'
-    ]
+  mode: "jit",
+  purge: {
+    content: ["./templates/**/*.html", "./theme/**/*.html"],
   },
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        theme: {
+          bg: "var(--color-bg)",
+          "bg-offset": "var(--color-bg-offset)",
+          text: "var(--color-text)",
+          "text-offset": "var(--color-text-offset)",
+          border: "var(--color-border)",
+          primary: "var(--color-primary)",
+          "primary-offset": "var(--color-primary-offset)",
+          secondary: "var(--color-secondary)",
+        },
+      },
+      minHeight: {
+        auto: "auto",
+      },
+      flex: {
+        2: "2 2 0%",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    container: false,
+  },
+};
